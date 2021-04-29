ec3486d9ff1494debc021467178ab5fee815a23a
yusuf ogundiji <ogundijiyusuf@gmail.com>
2021-04-29 16:54:49 +0100

    Add project files.

---
+++ netCoreQR/Controllers/QrCodeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netCoreQR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QrCodeController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("{encodeValue}")]
+        public IActionResult Get(string encodeValue)
+        {
+            if (string.IsNullOrEmpty(encodeValue))
+            {
+                encodeValue = "welcome user";
+            }
+            var image = QrCodeGenerator.GenerateByteArray(encodeValue);
+            return File(image, "image/jpeg");
+        }
+    }
+}
---
+++ netCoreQR/QrCodeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.IO;
+
+namespace netCoreQR
+{
+    public static class QrCodeGenerator
+    {
+        public static byte[] GenerateByteArray(string url)
+        {
+            var image = GenerateImage(url);
+            return ImageToByte(image);
+        }
+        public static Bitmap GenerateImage(string url)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(10);
+            return qrCodeImage;
+        }
+
+        private static byte[] ImageToByte(Image img)
+        {
+            using var stream = new MemoryStream();
+            img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            return stream.ToArray();
+        }
+
+    }
+}
